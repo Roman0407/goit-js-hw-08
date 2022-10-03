9de164f84059e02f44ffeb035f127431e12dd50f
Roman Stelmakh <105305762+Roman0407@users.noreply.github.com>
2022-10-04 00:21:18 +0300

    next

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,23 @@
 // Add imports above this line
-import SimpleLightbox from "simplelightbox";
+
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
-console.log(SimpleLightbox);
 console.log(galleryItems);
+
+
+const gallety = document.querySelector('.gallery');
+
+gallety.innerHTML = galleryItems.reduce(
+  (result, { preview, original, description }) =>
+    (result += `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`),
+  ''
+);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
